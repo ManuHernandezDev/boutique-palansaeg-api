71f17aa204df2342fcd2d805ec9945dce2999d8a
ManuHernandezDev <manu.hdz.dev@gmail.com>
2026-08-18 14:33:00 -0600

    first commit

---
+++ src/main/java/com/manuhdz/api/BoutiquePalansaegApiApplication.java
@@ -0,0 +1,13 @@
+package com.manuhdz.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoutiquePalansaegApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoutiquePalansaegApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/manuhdz/api/BoutiquePalansaegApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manuhdz.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoutiquePalansaegApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
